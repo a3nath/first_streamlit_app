66dff05210c08dd3b60ace12cd6f098ca3be30db
Amar Nath <49884655+a3nath@users.noreply.github.com>
2023-10-08 19:43:42 -0400

    Update streamlit_app.py
    import pandas and text file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('Snowflake healthy diner title')
 
 streamlit.header("snowflake badge 2 header")
 
 streamlit.text("snowflake badge 2 TEXT")
+
+streamlit.dataframe(my_fruit_list)
