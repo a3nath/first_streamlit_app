12e539856c56d0a0bd9e29235614d7bc6e2a2978
Amar Nath <49884655+a3nath@users.noreply.github.com>
2023-10-10 22:20:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,11 @@ import pandas as pd
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+fruits_to_show = my_fruit_list.loc[fruits_selected]√
 streamlit.title('Snowflake healthy diner title')
 streamlit.header("snowflake badge 2 header")
 streamlit.text("snowflake badge 2 TEXT")
