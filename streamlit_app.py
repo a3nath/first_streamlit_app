b1b8c8e07746e68406583692c161c459dbd0e8a1
Amar Nath <49884655+a3nath@users.noreply.github.com>
2023-10-10 23:31:22 -0400

    Update streamlit_app.py
    partial code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas as pd
+import requests
 import snowflake.connector
+from urllib.error import URLError
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -8,15 +10,37 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.title('Snowflake healthy diner title')
 streamlit.header("snowflake badge 2 header")
 streamlit.text("snowflake badge 2 TEXT")
 streamlit.dataframe(my_fruit_list)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
+try:
+  fruit_choice = streamlit.text_input("What fruit")
+  if not fruit_choice:
+    streamlit.error("Please select a fruit choice")
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+  
+def get_fruit_load_list():
+  with my_cxn.cursor() as my_cur:
+      my_cur.execute("select * from fruit_load_list")
+      return my_cur.fetchall()
+
+if streamlit.button("Get fruit list")
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_cur = my_cnx.cursor()
+  my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+  my_data_row = my_cur.fetchone()
+  streamlit.text("Hello from Snowflake:")
+  streamlit.text(my_data_row)
+
+
+
+
