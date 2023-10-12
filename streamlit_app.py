70665726f7ccc9a270c4a5e645633c8eafe67ac2
Amar Nath <49884655+a3nath@users.noreply.github.com>
2023-10-11 22:02:54 -0400

    Update streamlit_app.py
    commented

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,16 +19,14 @@ def get_fruity_vice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
-
-streamlit.header('Fruityyive Advice')
-try:
-  fruit_choice = streamlit.text_input('What fruit')
-  if not fruit_choice:
-    streamlit.error('Please select a fruit choice')
-  else:
-    back_from_function = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
-
+# streamlit.header('Fruityyive Advice')
+# try:
+#   fruit_choice = streamlit.text_input('What fruit')
+#   if not fruit_choice:
+#     streamlit.error('Please select a fruit choice')
+#   else:
+#     back_from_function = get_fruityvice_data(fruit_choice)
+#     streamlit.dataframe(back_from_function)
 # streamlit.header('the fruit load list contains:')
 # def get_fruit_load_list():
 #   with my_cnx.cursor() as my_cur:
