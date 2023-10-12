9315ec720fb6ce891cded442fda3783af8497fbb
Amar Nath <49884655+a3nath@users.noreply.github.com>
2023-10-11 22:20:07 -0400

    Update streamlit_app.py
    uncomment space

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,6 @@ try:
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
-
 # streamlit.header('the fruit load list contains:')
 # def get_fruit_load_list():
 #   with my_cnx.cursor() as my_cur:
