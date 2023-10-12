958d7b1ff6230e3df5ab75cef06c550029432cb6
Amar Nath <49884655+a3nath@users.noreply.github.com>
2023-10-11 21:58:51 -0400

    Update streamlit_app.py
    more syntax fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,22 +20,22 @@ def get_fruity_vice_data(this_fruit_choice):
   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
-streamlit.header("Fruityyive Advice")
+streamlit.header('Fruityyive Advice')
 try:
-  fruit_choice = streamlit.text_input("What fruit")
+  fruit_choice = streamlit.text_input('What fruit')
   if not fruit_choice:
-    streamlit.error("Please select a fruit choice")
+    streamlit.error('Please select a fruit choice')
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
 
-streamlit.header("the fruit load list contains")
+streamlit.header('the fruit load list contains:')
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
       my_cur.execute("select * from fruit_load_list")
       return my_cur.fetchall()
 
-if streamlit.button("Get fruit list"):
+if streamlit.button('Get fruit list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
@@ -45,8 +45,8 @@ def insert_row_snowflake(new_fruit):
       my_cur.execute("insert into fruit_load_list values('from streamlit')")
       return "Thanks for adding" + new_fruit
 
-add_my_fruit = streamlit.text_input("what fruit to add")
-if strean.button('Add a fruit to the list'):
+add_my_fruit = streamlit.text_input('what fruit to add')
+if streamlit.button('Add a fruit to the list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
