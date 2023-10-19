2788e325d91ad04eb1046912a3a780bebdd46f0c
Amar Nath <49884655+a3nath@users.noreply.github.com>
2023-10-18 22:12:20 -0400

    Update streamlit_app.py
    updated get_fruit function

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ streamlit.text("snowflake badge 2 TEXT")
 streamlit.dataframe(my_fruit_list)
                                            
 def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + this_fruit_choice)
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
